98e9ba7a919d31358f258bac963355cee9cc77a5
sergey <isergey207207@gmail.com>
2022-12-25 15:03:50 +0300

    Updated constants

---
+++ metar/metarConstantsEnglish.py
@@ -0,0 +1,97 @@
+intensivity = {
+    '-': 'Light',
+    '': 'Moderate',
+    '+': 'Heavy',
+    'VC': 'In the vicinity'
+}
+
+descriptor = {
+    'MI': 'Shallow',
+    'BC': 'Patches',
+    'PR': 'Partial',
+    'DR': 'Low drifting',
+    'BL': 'Blowing',
+    'SH': 'Shower',
+    'TS': 'Thunderstorm',
+    'FZ': 'Freezing'
+}
+
+precipitation = {
+    'DZ': 'Drizzle',
+    'RA': 'Rain',
+    'SN': 'Snow',
+    'SG': 'Snow grains',
+    'IC': 'Ice crystals',
+    'PL': 'Ice pellets',
+    'GR': 'Hail',
+    'GS': 'Small hail and/or snow pellets',
+    'UP': 'Unknown precipitation'
+}
+
+bad_visibility_weather_events = {
+    'BR': 'Mist',
+    'FG': 'Fog',
+    'FU': 'Smoke',
+    'VA': 'Volcanic ash',
+    'DU': 'Widespread dust',
+    'SA': 'Sand',
+    'HZ': 'Haze',
+}
+
+other_weather_events = {
+    'PO': 'Dust/sand whirls',
+    'SQ': 'Squalls',
+    'FC': 'Funnel cloud(s) (tornado or water spout)',
+    'SS': 'Sandstorm',
+    'DS': 'Duststorm'
+}
+
+RVR_prefixes = {
+    'L': 'Left',
+    'C': 'Center',
+    'R': 'Right'
+}
+
+RVR_visibilty_prefixes = {
+    'P': 'Longer then 2000 m',
+    'M': 'Shorter then minimal value'
+}
+
+RVR_visibility_changements_prefixes = {
+    'U': 'Increase of visibility',
+    'D': 'Decrease of visibility',
+    'N': 'No changes of visibility'
+}
+
+RVR_deposit = {
+    '0': 'dry and clear',
+    '1': 'damp',
+    '2': 'wet or water patches',
+    '3': 'rime or frost',
+    '4': 'dry snow',
+    '5': 'wet snow',
+    '6': 'slush',
+    '7': 'ice',
+    '8': 'compacled or rolled snow',
+    '9': 'frozen ruls or ridges'
+}
+
+RVR_extend_of_contamination = {
+    '1': '<10%',
+    '2': '11-25%',
+    '5': '26-51%',
+    '9': '51-100%',
+}
+
+cloudiness = {
+    "VV": "Vertical visibility",
+    "FEW": "Few",
+    "SCT": "Scallered",
+    "BKN": "Broken",
+    "OVC": "Overcast",
+    "NSC": "No significant clouds",
+    "NCD": "No clouds",
+    "SKC": "Sky clear",
+    "CB": "Сumulonimbus clouds",
+    "TCU": "Lowering cumulus"
+}
